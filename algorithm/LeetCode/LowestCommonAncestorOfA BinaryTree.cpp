cda16b507c6d933a0f564d6222e442fc1d1f1f6a
WangDrop <1208374893@qq.com>
2016-04-11 22:47:03 +0800

    add LCA

---
+++ algorithm/LeetCode/LowestCommonAncestorOfA BinaryTree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) return  NULL; //无其他节点，直接返回
+        if (root == p || root == q) return root; 
+        TreeNode * leftNode = lowestCommonAncestor(root->left, p, q);
+        TreeNode * rightNode = lowestCommonAncestor(root->right, p, q);
+        if (leftNode && rightNode) return root;　　//找到LCA，返回LCA
+        return leftNode ? leftNode : rightNode;　　
+    }
+};
