dca51ea98157eb86f4be3cae034f990e4002bec0
WangDrop <1208374893@qq.com>
2016-05-07 22:51:20 +0800

    subset II

---
+++ javaProject/javaTest.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: javaTest.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年05月07日 星期六 12时59分49秒
+ ************************************************************************/
+
+public class javaTest{
+    public static void main(String [] args){
+        System.out.println("Hello java!");
+    }
+}
